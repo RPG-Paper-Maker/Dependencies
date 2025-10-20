0a2584f8a9e862571aa64eb06350fa06af3b75cc
Marie Laporte <mmarie.laporte@gmail.com>
2025-10-20 22:41:16 +0200

    Update dependencies organisation

--- Updater/linux/Updater/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-path="$0"
-SCRIPTPATH=${path%????????????????}
-export LD_LIBRARY_PATH="${SCRIPTPATH}/Updater/libraries"
-export QT_QPA_PLATFORM_PLUGIN_PATH="${SCRIPTPATH}/Updater/libraries/plugins/platforms"
-export QT_PLUGIN_PATH="${SCRIPTPATH}/Updater/libraries/plugins"
-"${SCRIPTPATH}/Updater/Updater"
