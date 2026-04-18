e0b0875f669d7b4fda82828c8551186c8fa560e9
DESKTOP-2I00R5B\Marie <mmarie.laporte@gmail.com>
2026-04-18 17:00:06 +0200

    Update dist build

---
+++ dist/assets/ProjectUpdater_3_0_36-BV4qjGSF.js
@@ -0,0 +1,1 @@
+import{Dt as e,Pt as t,an as n}from"./ToasterError-Dyxe9FSQ.js";import{Gn as r}from"./Editor-Bd5eECw9.js";var i=class{static async update(){let i=r.current.getPath(),a=n.join(i,`Test`);await e(a)||await t(a)}};export{i as ProjectUpdater_3_0_36};
