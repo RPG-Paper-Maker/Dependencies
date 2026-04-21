b9cb1eddb0213358795cd6c24d9a7dcda70f065b
DESKTOP-2I00R5B\Marie <mmarie.laporte@gmail.com>
2026-04-21 22:43:22 +0200

    Update dist build

---
+++ dist/assets/ProjectUpdater_3_0_22-p7PJ3zNh.js
@@ -0,0 +1,1 @@
+import{Gt as e,Qt as t,an as n}from"./ToasterError-1ebwKr7e.js";import{Gn as r}from"./Editor-BekFMVP0.js";var i=class{static async update(){let i=r.current.getPath(),a=await e(n.join(i,`system.json`));if(a){for(let e of a.colors)e.a/=255;await t(n.join(i,`system.json`),a)}}};export{i as ProjectUpdater_3_0_22};
---
+++ dist/assets/ProjectUpdater_3_0_34-Doljrjzg.js
@@ -0,0 +1,1 @@
+import{Dt as e,Xt as t,an as n}from"./ToasterError-1ebwKr7e.js";import{Gn as r}from"./Editor-BekFMVP0.js";var i=class{static async update(){let i=r.current.getPath(),a=n.join(i,`backups`);await e(a)&&await t(a)}};export{i as ProjectUpdater_3_0_34};
