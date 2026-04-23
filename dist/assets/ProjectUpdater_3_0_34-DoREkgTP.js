0b1adfceba36534895be41200ab9c86aa64d6a15
DESKTOP-2I00R5B\Marie <mmarie.laporte@gmail.com>
2026-04-23 12:40:23 +0200

    Update dist build

---
+++ dist/assets/ProjectUpdater_3_0_34-DoREkgTP.js
@@ -0,0 +1,1 @@
+import{Dt as e,Xt as t,an as n}from"./ToasterError-BZ3-NPnv.js";import{Gn as r}from"./Editor-DwEmAyPs.js";var i=class{static async update(){let i=r.current.getPath(),a=n.join(i,`backups`);await e(a)&&await t(a)}};export{i as ProjectUpdater_3_0_34};
