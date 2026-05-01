6f0267dd37fdd9d04e67150a4dfa53990696af3d
DESKTOP-2I00R5B\Marie <mmarie.laporte@gmail.com>
2026-05-01 16:32:59 +0200

    Update dist build

---
+++ dist/assets/ProjectUpdater_3_0_34-CDkgSvph.js
@@ -0,0 +1,1 @@
+import{Dt as e,Zt as t,on as n}from"./index-BXSha7Io.js";import{Bn as r}from"./Editor-CQ4WIQUP.js";var i=class{static async update(){let i=r.current.getPath(),a=n.join(i,`backups`);await e(a)&&await t(a)}};export{i as ProjectUpdater_3_0_34};
