3d8993e6ae58eee8502bcc642b1d0e7b7daecdad
DESKTOP-2I00R5B\Marie <mmarie.laporte@gmail.com>
2026-05-02 22:06:19 +0200

    Update dist build

---
+++ dist/assets/ProjectUpdater_3_0_33-CjVLVZ19.js
@@ -0,0 +1,1 @@
+import{$t as e,Kt as t,fr as n,on as r}from"./index-oQtg8dML.js";import{Bn as i}from"./Editor-DUce7pFa.js";var a=class{static async update(){if(n.IS_DESKTOP){let n=i.current.getPath(),a=await t(r.join(n,`system.json`));a&&(a.pathDLCS=r.join(window.env.appPath,`DLCs`),await e(r.join(n,`system.json`),a))}}};export{a as ProjectUpdater_3_0_33};
