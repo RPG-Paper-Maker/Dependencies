d80b78fc990d408b982ab7f64188bbc19e5aed8b
DESKTOP-2I00R5B\Marie <mmarie.laporte@gmail.com>
2026-05-20 21:06:20 +0200

    Update dist build

---
+++ dist/assets/ProjectUpdater_3_0_22-B_7U-Nte.js
@@ -0,0 +1,1 @@
+import{$t as e,Kt as t,on as n}from"./index-BupSyfZ4.js";import{Bn as r}from"./Editor-BD-BK59q.js";var i=class{static async update(){let i=r.current.getPath(),a=await t(n.join(i,`system.json`));if(a){for(let e of a.colors)e.a/=255;await e(n.join(i,`system.json`),a)}}};export{i as ProjectUpdater_3_0_22};
